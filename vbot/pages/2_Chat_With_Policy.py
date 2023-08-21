b77feb0cd5641b61e248d36733033d63c9258236
adam xu <1254420756xyd@gmail.com>
2023-08-21 06:03:17 +0000

    update

--- vbot/pages/2_Chat_With_Policy.py
+++ vbot/pages/2_Chat_With_Policy.py
@@ -6,21 +6,21 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.llms import LlamaCpp
-from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.callbacks import StreamlitCallbackHandler
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+
+from utils.callbacks import StreamHandler, RetrievalHandler
 
 from config import (
     EMBEDDING,
     PROMPT_TEMPLATE,
-    LLM_13B,
-    LLM_7B,
+    LLM,
     SEARCH_KWARGS,
     SEARCH_TYPE,
-    CHUNK_SIZE
+    CHUNK_SIZE,
+    DB_BASE
 )
+
 
 PROMPT = PromptTemplate(
     input_variables=["context", "question"],
@@ -29,9 +29,6 @@ PROMPT = PromptTemplate(
 
 @st.cache_resource()
 def retriever(db_path: str):
-    # Load Embedding model from local path
-    # Load data from FAISS database to front end
-    # TODO: Change the EMBEDDING_MODEL to local path
     embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING)
 
     vector_db = FAISS.load_local(
@@ -47,40 +44,13 @@ def retriever(db_path: str):
 
     return retriever
 
-
-class StreamHandler(BaseCallbackHandler):
-    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
-        self.container = container
-        self.text = initial_text
-
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text += token
-        self.container.markdown(self.text)
-
-
-class PrintRetrievalHandler(BaseCallbackHandler):
-    def __init__(self, container):
-        self.container = container.expander("Context Retrieval")
-
-    # def on_retriever_start(self, query: str, **kwargs):
-    #     self.container.write(f"**Question:** {query}")
-
-    def on_retriever_end(self, documents, **kwargs):
-        # self.container.write(documents)
-        for idx, doc in enumerate(documents):
-            pprint(doc.metadata)
-            source = os.path.basename(doc.metadata["source"])
-            self.container.write(f"**Document {idx} from {source}**")
-            self.container.markdown(doc.page_content)
-
-
 # ======================APP==========================
 msgs = StreamlitChatMessageHistory()
 
 llm = LlamaCpp(
-    model_path=LLM_7B,
+    model_path=LLM,
     temperature=0.01,
-    n_ctx=2048,
+    n_ctx=3000,
     streaming=True,
     max_tokens=512
 )
@@ -89,7 +59,7 @@ chain_type_kwargs = {"prompt": PROMPT}
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=retriever('/workspace/gym/vbot/faiss/cs2048/multi-qa-mpnet-base-cos-v1'),
+    retriever=retriever(f'{DB_BASE}/cs{CHUNK_SIZE}/{os.path.basename(EMBEDDING)}'),
     chain_type_kwargs=chain_type_kwargs
 )
 
@@ -107,7 +77,7 @@ if user_query := st.chat_input(placeholder="Ask me about VW policy related quest
     st.chat_message("user").write(user_query)
 
     with st.chat_message("assistant"):
-        retrieval_handler = PrintRetrievalHandler(st.container())
+        retrieval_handler = RetrievalHandler(st.container())
         stream_handler = StreamHandler(st.empty())
 
         pprint(f"Start processing query: '{user_query}'")
---
+++ vbot/utils/callbacks.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from langchain.callbacks.base import BaseCallbackHandler
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+
+class RetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.container = container.expander("Context Retrieval")
+
+    # def on_retriever_start(self, query: str, **kwargs):
+    #     self.container.write(f"**Question:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        # self.container.write(documents)
+        for idx, doc in enumerate(documents):
+            pprint(doc.metadata)
+            source = os.path.basename(doc.metadata["source"])
+            self.container.write(f"**Document {idx} from {source}**")
+            self.container.markdown(doc.page_content)
